638d77b07cf5d32366545258e5cef8164d47f244
softwaiter <softwaiter@126.com>
2022-06-07 17:14:04 +0800

    :sparkles:实现在每一行的最末列增加两个按钮用于增加行和删除行

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DataGridOperationButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeight = 60;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column5,
+            this.Column4,
+            this.Column7});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.White;
+            this.dataGridView1.RowTemplate.Height = 60;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1710, 952);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            this.dataGridView1.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dataGridView1_RowsRemoved);
+            this.dataGridView1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView1_Scroll);
+            this.dataGridView1.SizeChanged += new System.EventHandler(this.dataGridView1_SizeChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "姓名";
+            this.Column1.MinimumWidth = 10;
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "性别";
+            this.Column2.MinimumWidth = 10;
+            this.Column2.Name = "Column2";
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "年龄";
+            this.Column3.MinimumWidth = 10;
+            this.Column3.Name = "Column3";
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "身份证号";
+            this.Column5.MinimumWidth = 10;
+            this.Column5.Name = "Column5";
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "家庭住址";
+            this.Column4.MinimumWidth = 10;
+            this.Column4.Name = "Column4";
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column7.HeaderText = "操作";
+            this.Column7.MinimumWidth = 10;
+            this.Column7.Name = "Column7";
+            this.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column7.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1734, 976);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewLinkColumn Column7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+namespace DataGridOperationButtons
+{
+    public partial class Form1 : Form
+    {
+        internal class ActionButtons
+        {
+            public ActionButtons(Button addButton, Button removeButton)
+            {
+                AddButton = addButton;
+                RemoveButton = removeButton;
+            }
+
+            public Button AddButton { get; set; }
+            public Button RemoveButton { get; set; }
+        }
+
+        List<ActionButtons> mButtons = new List<ActionButtons>();
+        int btnColIndex = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButtons()
+        {
+            int rowIndex = dataGridView1.Rows.Count - 1;
+            Rectangle rect = this.dataGridView1.GetCellDisplayRectangle(btnColIndex, rowIndex, false);
+
+            Button btnAdd = new Button();
+            btnAdd.Text = "+";
+            btnAdd.Click += onAddButtonClick;
+            dataGridView1.Controls.Add(btnAdd);
+
+            Button btnRemove = new Button();
+            btnRemove.Text = "-";
+            btnRemove.Click += onRemoveButtonClick;
+            dataGridView1.Controls.Add(btnRemove);
+
+            mButtons.Add(new ActionButtons(btnAdd, btnRemove));
+        }
+
+        private void RemoveButtons(int rowIndex)
+        {
+            if (rowIndex >= 0 && rowIndex < mButtons.Count)
+            {
+                ActionButtons ab = mButtons[rowIndex];
+                ab.AddButton.Visible = false;
+                ab.AddButton.Dispose();
+                ab.RemoveButton.Visible = false;
+                ab.RemoveButton.Dispose();
+
+                mButtons.RemoveAt(rowIndex);
+            }
+        }
+
+        private void HideAllActionButtons(ActionButtons ab)
+        {
+            dataGridView1.BeginInvoke(() =>
+            {
+                ab.AddButton.Visible = ab.RemoveButton.Visible = false;
+            });
+        }
+
+        private void UpdateActionButtonsPosition(ActionButtons ab, Rectangle rect, int rowIndex)
+        {
+            dataGridView1.BeginInvoke(() =>
+            {
+                ab.AddButton.Location = new Point(rect.Left + 5, rect.Top + 5);
+                ab.AddButton.Size = new Size(rect.Width / 2 - 10, rect.Height - 10);
+                ab.AddButton.Visible = true;
+
+                ab.RemoveButton.Location = new Point(ab.AddButton.Left + ab.AddButton.Width + 5, rect.Top + 5);
+                ab.RemoveButton.Size = ab.AddButton.Size;
+                ab.RemoveButton.Visible = rowIndex > 0;
+            });
+        }
+
+        private void RepositionActionButtons()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(100);
+
+                int firstRow = dataGridView1.FirstDisplayedScrollingRowIndex;
+                int lastRow = firstRow + dataGridView1.DisplayedRowCount(false);
+
+                for (int i = 0; i < mButtons.Count; i++)
+                {
+                    ActionButtons ab = mButtons[i];
+                    ab.AddButton.Tag = i;
+                    ab.RemoveButton.Tag = i;
+
+                    if (i >= firstRow && i <= lastRow)
+                    {
+                        Rectangle rect = this.dataGridView1.GetCellDisplayRectangle(btnColIndex, i, false);
+                        UpdateActionButtonsPosition(ab, rect, i);
+                    }
+                    else
+                    {
+                        HideAllActionButtons(ab);
+                    }
+                }
+            });
+        }
+
+        private void onAddButtonClick(object? sender, EventArgs e)
+        {
+            if (sender != null)
+            {
+                Button btn = (Button)sender;
+                int rowIndex = (int)btn.Tag;
+                if (rowIndex == dataGridView1.Rows.Count - 1)
+                {
+                    dataGridView1.Rows.Add();
+                }
+                else
+                {
+                    dataGridView1.Rows.Insert(rowIndex + 1, 1);
+                }
+            }
+        }
+
+        private void onRemoveButtonClick(object? sender, EventArgs e)
+        {
+            if (sender != null)
+            {
+                Button btn = (Button)sender;
+                int rowIndex = (int)btn.Tag;
+                dataGridView1.Rows.RemoveAt(rowIndex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            AddButtons();
+            RepositionActionButtons();
+        }
+
+        private void dataGridView1_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            RemoveButtons(e.RowIndex);
+            RepositionActionButtons();
+        }
+
+        private void dataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            RepositionActionButtons();
+        }
+
+        private void dataGridView1_SizeChanged(object sender, EventArgs e)
+        {
+            RepositionActionButtons();
+        }
+    }
+}
